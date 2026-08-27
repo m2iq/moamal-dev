4bc212b0283adcf4c5c1f07fd59e83c8c130d224
moamal <ooooi88ii@gmail.com>
2026-08-27 14:58:13 +0300

    hh

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Cairo } from "next/font/google";
 import "./globals.css";
 
@@ -21,7 +22,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ar" dir="rtl">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7553202956815424"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className={cairo.className}>{children}</body>
+
     </html>
   );
 }
